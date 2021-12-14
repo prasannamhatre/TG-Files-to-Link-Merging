39998b3ffa4e3790bb4d5f7f2bea2f670ac64104
prasannamhatre <64671720+prasannamhatre@users.noreply.github.com>
2021-12-14 22:11:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9", 
+        
     ],
     entry_points="""
         [console_scripts]
